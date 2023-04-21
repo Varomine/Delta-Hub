a554cfa815d54adae0c6efef5c40f9bdc547d37b
Varomine <114858965+Varomine@users.noreply.github.com>
2023-04-21 14:55:30 +0700

    Add files via upload

---
+++ Read me.lua
@@ -0,0 +1,13 @@
+--[[
+    ** Support Game **
+
+    World // Zero : 2727067538
+    Mining Simulator 2 : 9551640993
+    Arsenal : 286090429
+    Build a boat : 537413528
+    Lucky Blocks : 662417684
+
+    ** Discord **
+
+    Soon...
+]]
